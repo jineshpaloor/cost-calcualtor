7e142cf7885f7ceba4d0efa4bcc3c69a7828155e
jinesh <jineshpaloor@gmail.com>
2014-01-26 23:48:07 +0530

    static, templates replaced, settings updated

--- costcalculator/settings.py
+++ costcalculator/settings.py
@@ -1,4 +1,5 @@
 # Django settings for costcalculator project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -6,6 +7,8 @@ TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 MANAGERS = ADMINS
 
@@ -71,6 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'costcalculator/static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -173,5 +177,5 @@ STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'costcalculator/static'),
 )
