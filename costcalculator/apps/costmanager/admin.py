3fdc2baabaf4ce399fd406aeaad90406dfe3e1a8
jinesh <jineshpaloor@gmail.com>
2014-01-26 22:59:00 +0530

    static and templates files added

--- costcalculator/apps/costmanager/admin.py
+++ costcalculator/apps/costmanager/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from costcalculator.apps.costmanager.models import PersonalInfo,Group, \
         Category, SubCategory, Bill
 
 # Register your models here.
 admin.site.register(PersonalInfo)
-admin.site.register(Group)
+
+class UserInline(admin.StackedInline):
+    model = User
+    extra = 3
+
+class GroupAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['name']}),
+        ('Description', {'fields': ['description']}),
+        ('Members', {'fields': ['members'],
+                              'classes': ['collapse']}),
+    ]
+    #inlines = [UserInline]
+
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Category)
 admin.site.register(SubCategory)
 admin.site.register(Bill)
